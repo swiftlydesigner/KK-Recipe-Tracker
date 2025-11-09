1cad2b46dab5d953d314ecbb0d9759d31448df85
swiftlydesigner <98251445+swiftlydesigner@users.noreply.github.com>
2025-11-09 12:18:07 -0700

    Include all necessary code and write basic code

--- Main.py
+++ Main.py
@@ -3,3 +3,16 @@
 # Autumn 2025
 #
 # Due Nov 14, 2025 @ 19:00
+
+import Logic # Usage: Logic.func name
+from IoManager import IoManager # Usage: IoManager.method
+from RecipeManager import RecipeManager # Usage: RecipeManager().method
+
+def main():
+    input_str = IoManager.get_str("What is your name?")
+    input_str_non_empty = IoManager.get_str("What is your City?", is_empty=False)
+
+    print(f"Your name is {input_str}\nYour city is {input_str_non_empty}.")
+
+if __name__ == "__main__":
+    main()
