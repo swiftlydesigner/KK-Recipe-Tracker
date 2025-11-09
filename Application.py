a30fd010ac578ba150eadf95f227af536ab221e0
swiftlydesigner <98251445+swiftlydesigner@users.noreply.github.com>
2025-11-09 13:56:52 -0700

    Create Application.py

---
+++ Application.py
@@ -0,0 +1,15 @@
+# Created by Kyle Parker and Koa Hawn
+# COMP 3005 Python
+# Autumn 2025
+#
+# Due Nov 14, 2025 @ 19:00
+
+class Appplication:
+        ''' Attributes:
+
+        '''
+        def __init__(self):
+                pass
+
+        def runApp(self):
+                pass
