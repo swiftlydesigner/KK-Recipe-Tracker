54d334bda87f0c4b4b4eb77a21f8eef44fdd2226
swiftlydesigner <98251445+swiftlydesigner@users.noreply.github.com>
2025-11-09 13:10:34 -0700

    Fix methods within static class

--- IoManager.py
+++ IoManager.py
@@ -13,19 +13,19 @@ class IoManager:
         pass # No setup required
 
     @staticmethod
-    def _get_input(self, message):
+    def _get_input(message):
         ''' Gets input from the user with a pre-defined format.'''
         return input(f"{message}\n> ")
 
     @staticmethod
-    def get_str(self, message, empty_ok=True, error=False):
+    def get_str(message, empty_ok=True, error=False):
         '''Prints the message, and returns a valid string from the user.'''
 
         if error:
             print("Bad user input. Please enter a valid string.", end='')
             print("" if empty_ok else " Empty strings are INVALID.")
 
-        response = self._get_input(message)
+        response = IoManager._get_input(message)
 
         # Check if empty is acceptable and enforce rules
         if not empty_ok and len(response) == 0:
@@ -34,14 +34,14 @@ class IoManager:
         return response
 
     @staticmethod
-    def get_int(self, message, error=False):
+    def get_int(message, error=False):
         '''Will read an int from the user and return a valid integer.'''
 
         # Note to programmer: Use try/catch to convert into int and use similar pattern to read_str (call itself and set error to True. Also add error check adding the message as above.)
         pass
 
     @staticmethod
-    def get_float(self, message, error=False):
+    def get_float(message, error=False):
         '''Will read a float from the user and return a valid float.'''
         pass
 
