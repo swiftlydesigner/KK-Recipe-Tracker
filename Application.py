7b2975c790a320bbf686b5d8ea13d206678b89e6
swiftlydesigner <98251445+swiftlydesigner@users.noreply.github.com>
2025-11-11 21:01:14 -0700

    Migrate app flow to Application

--- Application.py
+++ Application.py
@@ -4,6 +4,10 @@
 #
 # Due Nov 14, 2025 @ 19:00
 
+import Logic # Usage: Logic.func name
+from IoManager import IoManager # Usage: IoManager.method
+from RecipeManager import RecipeManager # Usage: RecipeManager().method
+
 class Application:
         ''' Attributes:
 
@@ -12,4 +16,7 @@ class Application:
                 pass
 
         def runApp(self):
-                pass
+            input_str = IoManager.get_str("What is your name?")
+            input_str_non_empty = IoManager.get_str("What is your City?", empty_ok=False)
+
+            print(f"Your name is {input_str}\nYour city is {input_str_non_empty}.")
--- Main.py
+++ Main.py
@@ -4,15 +4,11 @@
 #
 # Due Nov 14, 2025 @ 19:00
 
-import Logic # Usage: Logic.func name
-from IoManager import IoManager # Usage: IoManager.method
-from RecipeManager import RecipeManager # Usage: RecipeManager().method
+from Application import Application
 
 def main():
-    input_str = IoManager.get_str("What is your name?")
-    input_str_non_empty = IoManager.get_str("What is your City?", empty_ok=False)
-
-    print(f"Your name is {input_str}\nYour city is {input_str_non_empty}.")
+    app = Application()
+    app.runApp()
 
 if __name__ == "__main__":
     main()
