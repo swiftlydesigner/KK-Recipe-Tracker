00475031396ec44b019bfecece08786817ebe79f
swiftlydesigner <98251445+swiftlydesigner@users.noreply.github.com>
2025-11-13 21:59:20 -0700

    Implement RecipeFactory

--- RecipeFactory.py
+++ RecipeFactory.py
@@ -1,3 +1,8 @@
+from datetime import datetime, time
+
+from MasterTime import MasterTime
+from Recipe import Recipe
+
 
 class RecipeFactory:
     @staticmethod
@@ -8,18 +13,33 @@ class RecipeFactory:
 
             name = psv_separated[0]
             chef = psv_separated[1]
-            # self.__name = name
-            # self.__chef = chef
-            # self.__ingredients = ingredients
-            # self.__tools = tools
-            # self.__rating = rating
-            # self.__cooking_time = cooking_time
-            # self.__prep_time = prep_time
-            # self.__total_time = total_time
-            # self.__date_created = date_created if not None else datetime.now()
-            # self.__date_updated = date_updated if not None else datetime.now()
+            ingredients = psv_separated[2]
+            tools = psv_separated[3]
+            rating = int(psv_separated[4])
+            cook_time = RecipeFactory.__time_from_str(psv_separated[5])
+            prep_time = RecipeFactory.__time_from_str(psv_separated[6])
+            total_time = RecipeFactory.__time_from_str(psv_separated[7])
+            date_created = RecipeFactory.__ts_from_str(psv_separated[8])
+            date_updated = RecipeFactory.__ts_from_str(psv_separated[9])
+
+            return Recipe(name, chef, ingredients, tools, rating,
+                          cook_time, prep_time, total_time,
+                          date_created, date_updated)
+
+        except ValueError:
+            raise RecipeMalformedError("Expected rating to be type int!", 0x02)
         except:
             raise RecipeMalformedError("Input recipe list was malformed!", 0x01)
+
+    @staticmethod
+    def __time_from_str(string):
+        time_format = "%H:%M:%S"
+        dt_data = datetime.strptime(string, time_format)
+        return dt_data.time()
+
+    @staticmethod
+    def __ts_from_str(ts):
+        return MasterTime.ts_from_str(ts)
 
 
 class RecipeMalformedError(Exception):
