ae15b5169bebc2b4224c781e281024d8e224c284
swiftlydesigner <98251445+swiftlydesigner@users.noreply.github.com>
2025-11-13 22:22:44 -0700

    Parse lists and update func name

--- RecipeFactory.py
+++ RecipeFactory.py
@@ -1,26 +1,27 @@
 from datetime import datetime, time
 
+from Ingredient import Ingredient
 from MasterTime import MasterTime
 from Recipe import Recipe
 
 
 class RecipeFactory:
     @staticmethod
-    def create_from_array(arr):
+    def create_from_psv_line(line):
         """Could raise RecipeMalformedError"""
         try:
-            psv_separated = arr.split("|")
+            psv_separated = line.split("|")
 
-            name = psv_separated[0]
-            chef = psv_separated[1]
-            ingredients = psv_separated[2]
-            tools = psv_separated[3]
-            rating = int(psv_separated[4])
-            cook_time = RecipeFactory.__time_from_str(psv_separated[5])
-            prep_time = RecipeFactory.__time_from_str(psv_separated[6])
-            total_time = RecipeFactory.__time_from_str(psv_separated[7])
-            date_created = RecipeFactory.__ts_from_str(psv_separated[8])
-            date_updated = RecipeFactory.__ts_from_str(psv_separated[9])
+            name = psv_separated[0].strip()
+            chef = psv_separated[1].strip()
+            ingredients = RecipeFactory.__gen_list_ingredients(psv_separated[2].strip())
+            tools = RecipeFactory.__list_from_csv(psv_separated[3].strip())
+            rating = float(psv_separated[4].strip())
+            cook_time = RecipeFactory.__time_from_str(psv_separated[5].strip())
+            prep_time = RecipeFactory.__time_from_str(psv_separated[6].strip())
+            total_time = RecipeFactory.__time_from_str(psv_separated[7].strip())
+            date_created = RecipeFactory.__ts_from_str(psv_separated[8].strip())
+            date_updated = RecipeFactory.__ts_from_str(psv_separated[9].strip())
 
             return Recipe(name, chef, ingredients, tools, rating,
                           cook_time, prep_time, total_time,
@@ -40,6 +41,35 @@ class RecipeFactory:
     @staticmethod
     def __ts_from_str(ts):
         return MasterTime.ts_from_str(ts)
+
+    @staticmethod
+    def __gen_list_ingredients(ingredients):
+        ingredient_list = []
+
+        for ingredient in ingredients.split(","):
+            result = RecipeFactory.__ingredient_from_str(ingredient)
+            if result is not None:
+                ingredient_list.append(result)
+
+        return ingredient_list
+
+    @staticmethod
+    def __ingredient_from_str(ingredient):
+        ingredient_obj = None # dead store, but easier to read
+        parts = ingredient.split("?")
+
+        if len(parts) == 3:
+            ingredient_obj = Ingredient(parts[0], float(parts[1]), parts[2])
+        elif len(parts) == 2:
+            ingredient_obj = Ingredient(parts[0], float(parts[1]), None)
+        elif len(parts) == 1:
+            ingredient_obj = Ingredient(parts[0], None, None)
+
+        return ingredient_obj
+
+    @staticmethod
+    def __list_from_csv(string):
+        return string.split(",")
 
 
 class RecipeMalformedError(Exception):
